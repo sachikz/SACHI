67eccfb3549738ab376283a85817bc89ae1f59f6
yuki <sachikz01@gmail.com>
2018-12-03 10:21:36 +0900

    domain:      'herokuapp.com'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,10 +76,10 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       address:     'smtp.gmail.com',
       port:        587,
-      domain:      'gmail.com',
+      domain:      'herokuapp.com',
       user_name:   ENV['EMAIL_ADDRESS'],
       password:    ENV['EMAIL_PASSWORD'],
-      authentication:  'login',
+      authentication:  'plain',
       enable_starttls_auto: true
     }
 
