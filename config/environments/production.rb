9f1b94dfcb8dee873e5e8075774ed796884da079
yuki <sachikz01@gmail.com>
2018-12-03 10:07:09 +0900

    production.rb edit --authentication:  'login'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
       domain:      'gmail.com',
       user_name:   ENV['EMAIL_ADDRESS'],
       password:    ENV['EMAIL_PASSWORD'],
-      authentication:  'plain',
+      authentication:  'login',
       enable_starttls_auto: true
     }
 
